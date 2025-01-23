46bbb3c2d207d0a4fcb2dac8be8ad2502901d8c8
Kevin Stewart <kevin.g.stewart@gmail.com>
2025-01-23 16:48:30 -0500

    Update user-coaching-installer.sh

--- user-coaching-installer.sh
+++ user-coaching-installer.sh
@@ -12,7 +12,7 @@ fi
 ## Create iFile System object (user-coaching-html)
 echo "..Creating the iFile system object for the user-coaching-html"
 curl -sk \
--u admin:admin \
+-u ${BIGUSER} \
 -H "Content-Type: application/json" \
 -d '{"name": "user-coaching-html", "source-path": "https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-html"}' \
 https://localhost/mgmt/tm/sys/file/ifile/ -o /dev/null
@@ -20,7 +20,7 @@ https://localhost/mgmt/tm/sys/file/ifile/ -o /dev/null
 # ## Create iFile LTM object (user-coaching-html)
 echo "..Creating the iFile LTM object for the user-coaching-html"
 curl -sk \
--u admin:admin \
+-u ${BIGUSER} \
 -H "Content-Type: application/json" \
 -d '{"name":"user-coaching-html", "file-name": "user-coaching-html"}' \
 https://localhost/mgmt/tm/ltm/ifile -o /dev/null
@@ -30,7 +30,7 @@ echo "..Creating the user-coaching-rule iRule"
 rule=$(curl -sk https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
 data="{\"name\":\"user-coaching-rule\",\"apiAnonymous\":\"${rule}\"}"
 curl -sk \
--u admin:admin \
+-u ${BIGUSER} \
 -H "Content-Type: application/json" \
 -d "${data}" \
 https://localhost/mgmt/tm/ltm/rule -o /dev/null
@@ -40,7 +40,7 @@ echo "..Creating the user-coaching-ja4t-rule iRule"
 rule=$(curl -sk https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-ja4t-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
 data="{\"name\":\"user-coaching-ja4t-rule\",\"apiAnonymous\":\"${rule}\"}"
 curl -sk \
--u admin:admin \
+-u ${BIGUSER} \
 -H "Content-Type: application/json" \
 -d "${data}" \
 https://localhost/mgmt/tm/ltm/rule -o /dev/null
@@ -48,7 +48,7 @@ https://localhost/mgmt/tm/ltm/rule -o /dev/null
 ## Create SSLO User-Coaching Inspection Service
 echo "..Creating the SSLO user-coaching inspection service"
 curl -sk \
--u admin:admin \
+-u ${BIGUSER} \
 -H "Content-Type: application/json" \
 -d "$(curl -sk https://raw.githubusercontent.com/kevingstewart/sslo-user-coaching/refs/heads/main/user-coaching-service)" \
 https://localhost/mgmt/shared/iapp/blocks -o /dev/null
